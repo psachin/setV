f8911c46b1bc6878739ba6a3033909a00f4b9d34
Sachin Patil <sacpatil@redhat.com>
2016-03-13 18:42:20 +0530

    Bug: Can't source from script.
    Need to manually source ~/.bashrc

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 # License: GNU GPL v3, See LICENSE file
 #
 
-# TODO: define colors
 BOLD_GREEN="\033[1m"
 RED="\e[0;31m"
 BLUE="\e[0;34m"
@@ -13,11 +12,9 @@ RESET="\e[0m"
 echo -e "${RED}"* "${RESET}""${BOLD_GREEN}"Creating directory to hold all Python virtual environments"${RESET}"
 mkdir -p "${HOME}"/virtualenvs
 
-# TODO: download actual file as ${HOME}/.virtual.sh
 echo -e "${RED}"* "${RESET}""${BOLD_GREEN}"Downloading setV"${RESET}"
 curl -# https://raw.githubusercontent.com/psachin/setV/master/virtual.sh -o ${HOME}/.virtual.sh
 
-# TODO: append "source ${HOME}/.virtual.sh" to ~/.bashrc or ~/.bash_profile
 if [ -e "${HOME}/.bashrc" ];
 then
     echo -e "${RED}"* "${RESET}""${BOLD_GREEN}"Appending "${BLUE}"~/.bashrc"${RESET}"
@@ -28,14 +25,13 @@ then
     echo "source ~/.virtual.sh" >> ${HOME}/.bash_profile
 fi
 
-# TODO: source
-source ~/.virtual.sh
-
 # TODO: verify
 
 # Done installing
 echo -e "${RED}"* "${RESET}""${BOLD_GREEN}"Installation done!"${RESET}"
+echo -e "${RED}"* "${RESET}"Type 'source ~/.bashrc'
+echo -e "${RED}"* "${RESET}"or open new terminal to start using "${BOLD_GREEN}"setV"${RESET}"
 
-# TODO: print usage
+# Usage
 echo -e "${RED}"===================="${RESET}"
 echo -e "${BOLD_GREEN}"Usage: setv VIRTUAL_ENVIRONMENT_NAME"${RESET}"
